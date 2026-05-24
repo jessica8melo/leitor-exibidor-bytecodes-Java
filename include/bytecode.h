59376e2b5d20dc0370a2ca9931c370996197d779
natliaraujo <natalia.aaraujo@hotmail.com>
2026-05-23 21:54:56 -0300

    modified:   include/bytecode.h 	modified:   src/bytecodes.c 	modified:   src/special_instructions.c

--- include/bytecode.h
+++ include/bytecode.h
@@ -1,4 +1,23 @@
 #ifndef BYTECODE_H
 #define BYTECODE_H
 
+#include "classfile.h"
+#include <stdio.h>
+
+typedef struct{
+
+    char *mnemonic;
+    int operands;
+
+}Opcode;
+
+extern Opcode opcodeTable[256];
+
+void print_bytecodes(
+        ClassFile *cf,
+        u1 *code,
+        u4 code_length,
+        FILE *out
+);
+
 #endif
