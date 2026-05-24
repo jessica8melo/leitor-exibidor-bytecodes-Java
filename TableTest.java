99dae9dd26a982b9eaf48b6de43999913790cea3
natliaraujo <natalia.aaraujo@hotmail.com>
2026-05-23 22:53:16 -0300

    new file:   TableTest.class 	new file:   TableTest.java 	modified:   bin 	modified:   src/bytecodes.o

---
+++ TableTest.java
@@ -0,0 +1,25 @@
+public class TableTest{
+
+    public static void main(String args[]){
+
+        int x=2;
+
+        switch(x){
+
+            case 1:
+                break;
+
+            case 2:
+                break;
+
+            case 3:
+                break;
+
+            default:
+                break;
+
+        }
+
+    }
+
+}
